896001ebdf60700d3f91dddf6261aa077db24c1b
marcoslafoz <marcos.lafoz96@gmail.com>
2023-01-23 11:04:29 +0100

    23/01/2023 - 11:04

---
+++ Tema6/PlantillaT6.java
@@ -0,0 +1,13 @@
+package Tema6;
+
+import java.util.Scanner;
+
+public class PlantillaT6 /*Cambiar*/ {
+    public static void main(String[] args) {
+        try (Scanner teclado = new Scanner(System.in)) {
+            
+            //Codigo aqui
+
+        }
+    }
+}
