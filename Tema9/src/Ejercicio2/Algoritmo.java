aea66286841538455f51fc97e245a8d96f97acda
marcoslafoz <marcos.lafoz96@gmail.com>
2023-03-27 12:34:20 +0200

    27/03/2023 - 12:34

--- Tema9/src/Ejercicio2/Algoritmo.java
+++ Tema9/src/Ejercicio2/Algoritmo.java
@@ -1,13 +1,44 @@
 package Ejercicio2;
 
+import java.util.Random;
+
 public class Algoritmo {
 	
-	public static int buscar(int[] vector1, int elemento) {
-		
-		
-		
-		return  1;
+    //Llenamos el vector con numeros aleatorios
+    public static void llenarVector(int[] vector, Random rand, int TAMANO_ALEATORIOS) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = rand.nextInt(TAMANO_ALEATORIOS);
+        }
+    }
 	
-	}
+    //Imprimimos el vector
+    public static void imprimirVector(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    //Ordenamos el vector
+    public static void ordenarVector(int[] vector) {
+        int aux, cont0, cont1;
+
+        for (int a = 0; a < vector.length; a++) {
+            cont0 = 0;
+            cont1 = 1;
+
+            for (int i = 0; i < vector.length; i++) {
+                if (cont1 < vector.length) {
+                    if (vector[cont0] > vector[cont1]) {
+                        aux = vector[cont0];
+                        vector[cont0] = vector[cont1];
+                        vector[cont1] = aux;
+                    }
+                }
+                cont0++;
+                cont1++;
+            }
+        }
+    }
 
 }
--- Tema9/src/Ejercicio2/Main.java
+++ Tema9/src/Ejercicio2/Main.java
@@ -4,50 +4,34 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Definomis tamaño del vector y vector
-        int[] vector1 = new int[10];
-
-        // Objeto random
+        
+    	/*
+    	 * @param tAleatorios , tVector
+    	 * @author Marcos
+    	 * */
+    	
+    	final int TAMANO_ALEATORIOS = 100 , TAMANO_VECTOR = 20 ;
+    	
+    	//Instanciamos el vector
+    	int[] vector1 = new int[TAMANO_VECTOR];
+    	
+    	//Creamoso el objeto para los numeros aleatorios
         Random rand1 = new Random();
 
-        // Bucle ordenado
-        System.out.println("Bucle desordenado");
-        for (int i = 0; i < vector1.length; i++) {
-            vector1[i] = rand1.nextInt(100);
-            System.out.print(vector1[i] + " ");
-        }
+        // Llenar el vector con números aleatorios
+        Algoritmo.llenarVector(vector1, rand1, TAMANO_ALEATORIOS);
 
-        // Aux
-        int aux, cont0, cont1;
+        System.out.println("Vector desordenado:");
+        Algoritmo.imprimirVector(vector1);
 
-        // code
-        for (int a = 0; a < vector1.length; a++) {
-            cont0 = 0;
-            cont1 = 1;
-            for (int i = 0; i < vector1.length; i++) {
+        // Ordenar el vector
+        Algoritmo.ordenarVector(vector1);
 
-                if (cont1 < vector1.length) {
-
-                    if (vector1[cont0] > vector1[cont1]) {
-                        aux = vector1[cont0];
-                        vector1[cont0] = vector1[cont1];
-                        vector1[cont1] = aux;
-                    }
-                }
-
-                cont0++;
-                cont1++;
-            }
-
-        }
-
-        System.out.println("\nBucle ordenado");
-
-        // Bucle ordenado
-        for (int i = 0; i < vector1.length; i++) {
-            System.out.print(vector1[i] + " ");
-        }
-
+        System.out.println("\nVector ordenado:");
+        Algoritmo.imprimirVector(vector1);
     }
+
+
+
+
 }
