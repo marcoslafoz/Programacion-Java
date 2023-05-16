ab651b4c67e6bf9ec8e2931606b969534f579f6c
marcoslafoz <marcos.lafoz96@gmail.com>
2023-05-16 13:18:48 +0200

    Tema13 (Ejercicio1) - 16/05/2023 - 13:18

---
+++ Tema13/src/Ejercicio1/Main.java
@@ -0,0 +1,34 @@
+package Ejercicio1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        try {
+            
+        	BufferedWriter writer = new BufferedWriter(new FileWriter("primos.txt"));
+            
+        	writer.write("Lista de numeros primos del 1 al 500\n");
+        	writer.newLine();
+        	
+            for (int numero = 1; numero <= 500; numero++) {
+                if (Primos.esPrimo(numero)) {
+                    writer.write(Integer.toString(numero));
+                    writer.newLine();
+                }
+            }
+            
+            writer.close();
+            
+            System.out.println("Los números primos se han escrito en el archivo primos.txt.");
+            
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo primos.txt: " + e.getMessage());
+        }
+        
+        
+    }
+}
